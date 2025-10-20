21d3ed4ef7f57de82ddd5c64030587cb16026d5b
aydbtw371 <a.dimitrov2021@pmg-pernik.com>
2025-10-20 08:36:08 +0300

    JS 20.10.25

---
+++ numberToTen.js
@@ -0,0 +1,15 @@
+
+
+    function drawStars(bottom = 5) {
+  
+  if (bottom <= 0) return '';
+  if (bottom % 2 === 0) bottom = bottom - 1;
+
+  const lines = [];
+  for (let stars = 1; stars <= bottom; stars += 1) {
+    const spaces = (bottom - stars) / 2;
+    lines.push(' '.repeat(spaces) + '*'.repeat(stars));
+  }
+  return lines.join('\n');
+}
+drawStars();
